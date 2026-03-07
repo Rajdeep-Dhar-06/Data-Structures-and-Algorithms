c89e4f093dcba45c4663460028a7febbf45ab250
Rajdeep Dhar <rajdeepdhar.dev@gmail.com>
2026-03-07 12:02:34 +0530

    Time: 35 ms (44.4%), Space: 22.8 MB (48.8%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    int zeroMove(string s, int st) {
-        string x = s;
-        int ans = 0;
-        for (auto ch : s) {
-            if (ch - '0' != st)
-                ans++;
-            st = 1 - st;
-        }
-        return ans;
-    }
-    int oneMove(string s, int st) {
-        string x = s.substr(1) + s[0];
-        int ans = 0;
-        for (auto ch : s) {
-            if (ch - '0' != st)
-                ans++;
-            st = 1 - st;
-        }
-        return ans;
-    }
-    int twoMove(string s, int st) {
-        string x = s.substr(2) + s[0] + s[1];
-        int ans = 0;
-        for (auto ch : s) {
-            if (ch - '0' != st)
-                ans++;
-            st = 1 - st;
-        }
-        return ans;
-    }
     int minFlips(string s) {
-        return min({zeroMove(s, 0), zeroMove(s, 1), oneMove(s, 0),
-                   oneMove(s, 1),  twoMove(s, 0),  twoMove(s, 1)});
+        int n = s.size();
+        s += s;
+        string x = "";
+        for(int i=0; i<2*n; i++){
+            x += i%2 ? '1' : '0';
+        }
+        int curr = 0, ans = 1e9;
+        for(int i=0; i<2*n; i++){
+            if(s[i] != x[i]) curr++;
+            if(i>=n){
+                if(s[i-n] != x[i-n]) curr--;
+            }
+            if(i>=n-1)
+                ans = min({ans, curr, n-curr});
+        }
+        return ans;
     }
 };
