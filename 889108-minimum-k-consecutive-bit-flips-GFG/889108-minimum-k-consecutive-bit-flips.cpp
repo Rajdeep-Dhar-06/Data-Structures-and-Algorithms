4a4e5c1063456ea8c9b207c5065773e5738f5121
Rajdeep Dhar <rajdeepdhar.dev@gmail.com>
2026-03-12 20:23:30 +0530

    Added solution - LeetHub

--- 889108-minimum-k-consecutive-bit-flips-GFG/889108-minimum-k-consecutive-bit-flips.cpp
+++ 889108-minimum-k-consecutive-bit-flips-GFG/889108-minimum-k-consecutive-bit-flips.cpp
@@ -3,14 +3,14 @@ class Solution {
     int kBitFlips(vector<int>& arr, int k) {
         int sum = 0, ops = 0;
         int n = arr.size();
-        vector<int> diff(n, 0);
+        vector<int> diff(n+1, 0);
         for(int i=0; i<n; i++){
             sum += diff[i];
             int x = (sum + arr[i] + 2)%2;
             if(x==0){
                 if(i+k>n) return -1;
                 sum++;
-                if(i+k<n) diff[i+k] -= 1;
+                diff[i+k] -= 1;
                 ops++;
             }
         }
