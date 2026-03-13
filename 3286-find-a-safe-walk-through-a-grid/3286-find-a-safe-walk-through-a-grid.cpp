bdc127b188a3925f4b110bdb3ca913687e1667c2
Rajdeep Dhar <rajdeepdhar.dev@gmail.com>
2026-03-13 23:38:14 +0530

    Time: 16 ms (70.17%), Space: 36.7 MB (40.13%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,53 @@
+#define F first
+#define S second
+class Solution {
+public:
+    vector<vector<int>> g;
+    vector<vector<int>> dist;
+    vector<vector<int>> vis;
+    int n, m;
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+    bool isValid(int x, int y) { return (x >= 0 && x < n && y >= 0 && y < m); }
+    int bfs(int i, int j) {
+        deque<pair<int, pair<int, int>>> dq;
+        dist[i][j] = g[i][j]==0 ? 0 : 1;
+        dq.push_back({dist[i][j], {i, j}});
+
+        while (!dq.empty()) {
+            auto [d, u] = dq.front();
+            auto [x, y] = u;
+            dq.pop_front();
+
+            if(vis[x][y]) continue;
+            vis[x][y] = 1;
+            // cout << x << " " << y << endl;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (isValid(nx, ny) && !vis[nx][ny]) {
+                    // cout << nx << " " << ny << endl;
+                    if (g[nx][ny] == 1) {
+                        dist[nx][ny] = min(dist[nx][ny], 1 + d);
+                        dq.push_back({dist[nx][ny], {nx, ny}});
+                    } else {
+                        dist[nx][ny] = min(dist[nx][ny], d);
+                        dq.push_front({dist[nx][ny], {nx, ny}});
+                    }
+                }
+            }
+        }
+        cout << dist[n - 1][m - 1] << endl;
+        return dist[n - 1][m - 1];
+    }
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        g = grid;
+        n = grid.size();
+        m = grid[0].size();
+        dist.resize(n + 1, vector<int>(m + 1, 1e9));
+        vis.resize(n + 1, vector<int>(m + 1, 0));
+        return bfs(0, 0) < health;
+    }
+};
