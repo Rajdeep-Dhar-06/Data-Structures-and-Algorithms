250f21859131069b7081b51bc7a349444be12a02
Rajdeep Dhar <rajdeepdhar.dev@gmail.com>
2026-03-13 18:55:32 +0530

    Time: 28 ms (55.48%), Space: 29 MB (65.81%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool check(long long mid, int mh, vector<int>& wt){
-        long long h = 0;
-        for(int i=0; i<wt.size(); i++){
-            int time = wt[i];
-            long long c = (mid*2LL)/time;
-            long long x = floor(((sqrt(1+4*c))-1)/2);
-            h += x;
+    bool check(long long mid, long long mh, vector<int>& wt) {
+        for (int i = 0; i < wt.size(); i++) {
+            long long c = (mid * 2LL) / wt[i];
+            long long x = floor(((sqrt(1 + 4 * c)) - 1) / 2LL);
+            mh -= x;
         }
-        return mh-h <= 0;
+        return mh <= 0;
     }
     long long minNumberOfSeconds(int mh, vector<int>& wt) {
-        long long lo = 0, hi = 1e18, ans;
-        while(lo <= hi){
-            long long mid = (lo+hi)/2;
-            if(check(mid, mh, wt)){
+        long long limit = (mh * (mh + 1LL)) / 2;
+        long long lo = 0, hi = *max_element(wt.begin(), wt.end())*limit, ans;
+        while (lo <= hi) {
+            long long mid = (lo + hi) / 2;
+            if (check(mid, mh, wt)) {
                 ans = mid;
                 hi = mid - 1;
-            }
-            else{
+            } else {
                 lo = mid + 1;
             }
         }
